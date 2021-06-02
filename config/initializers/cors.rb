5e709898802bc5d751f86d9a6bc8e9f21be6e618
lucien andrieux <lucien.andrieux@epfedu.fr>
2021-06-02 09:12:22 +0200

    heroku cors.rb url added for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,9 +4,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
 credentials: true
   end
-#   allow do
-#     origins "http://localhost:3035"
-#     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-# credentials: true
-#   end
+  allow do
+    origins "https://rails-react-cat.herokuapp.com/"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+credentials: true
+  end
 end
